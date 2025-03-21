9bfce99a87d5817cc74c13d73a9983118c7e168a
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2025-03-21 09:28:54 -0300

    Update font.ts

--- src/app/ui/fonts/font.ts
+++ src/app/ui/fonts/font.ts
@@ -1,5 +1,12 @@
 import { Poppins } from "next/font/google";
 import { Fira_Code } from "next/font/google";
 
-export const poppins = Poppins({weight: ["400", "700"], subsets: ['latin'] })
-export const fira_code = Fira_Code({weight: ["400", "700"], subsets: ['latin'] })
+export const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const fira_code = Fira_Code({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
