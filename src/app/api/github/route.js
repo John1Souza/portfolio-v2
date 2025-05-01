4409bdda1155cc31f9caa11d60c0e0dc2b5d09eb
Johnatas Pereira de Souza <johnatasuitec@gmail.com>
2025-05-01 14:51:58 -0300

    feat: fix url

--- src/app/api/github/route.js
+++ src/app/api/github/route.js
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const token = process.env.PRIVATE_GITHUB_ACCESS_TOKEN;
-  const userName = process.env.PRIVATE_GITHUB_USER_NAME;
 
   try {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos?page=1&per_page=100`, {
+    const response = await fetch(`https://api.github.com/users/John1Souza/repos?page=1&per_page=100`, {
       headers: {
         'Authorization': `token ${token}`,
         'Accept': 'application/vnd.github.v3+json'
